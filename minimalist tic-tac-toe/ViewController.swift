f4f33ec7eae1c59e2dd447267f705e5d667dda30
jmprovencher <capucine55>
2017-04-30 16:47:52 -0400

    started logic and design for tic-tac-toe game. TODO: fix layout problem on other devices

--- minimalist tic-tac-toe/ViewController.swift
+++ minimalist tic-tac-toe/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var activePlayer = 1
+    var gameState = [Int]( repeating: 0, count: 9 )
+    
+    @IBAction func caseTap(_ sender: AnyObject) {
+        if(gameState[sender.tag-1] == 0){
+            gameState[sender.tag-1] = activePlayer
+            if(activePlayer==1){
+    sender.setImage(UIImage(named:"Cross.png"), for: UIControlState())
+                activePlayer = 2
+            }
+            else{
+    sender.setImage(UIImage(named:"Nought.png"), for: UIControlState())
+                activePlayer = 1
+            }
+            
+            
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
